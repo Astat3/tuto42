4715892c13b9e473eeffa2c889518d3387179da4
Adam Gallot <agallot@f1r5s21.clusters.42paris.fr>
2025-09-11 11:19:27 +0200

    strings

---
+++ string/string.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+
+typedef char * string;/*is the way to add string to that library*/
+
+int main(void){
+
+string s = "Hello";
+printf("%s",s);
+
+return 0;
+	}
